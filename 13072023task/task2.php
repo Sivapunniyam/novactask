69a75b4751c19b64f432cb260fdd24204a54fc7d
sivapunniyam <shivapunniyam@gmail.com>
2023-07-14 12:41:27 +0530

    14072023- Second commit

--- 13072023task/task2.php
+++ 13072023task/task2.php
@@ -2,7 +2,7 @@
 $str = file_get_contents('new.json');
 $array = json_decode($str, true);
 $new = array_filter($array, function ($var) {
-    return ($var['salary'] > 23000 );
+    return ($var['salary'] < 23000 );
 });
 print_r($new);
 ?>
